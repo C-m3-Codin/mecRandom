aca37960df1d7257df938f55b49062ef024f9345
c1337 <cyril199897@gmail.com>
2022-04-21 14:19:45 +0530

    some commit

---
+++ lib/HomeScreen/OfferRide/RideRequestClass/rideRequest.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+//class to represent each user making a request
+class RideRequest {
+  final String requestedUserId;
+  final String destination;
+  final String time;
+
+  RideRequest({
+    required this.requestedUserId,
+    required this.destination,
+    required this.time,
+  });
+}
---
+++ lib/MyRequests/myRequestClass.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+//class to represent each user making a request
+class MyRequest {
+  final String requestId;
+  final String destination;
+  final String time;
+
+  MyRequest({
+    required this.requestId,
+    required this.destination,
+    required this.time,
+  });
+}
